dbd9a6a28fa6a22a5ff3396a0d248f9c021cc0a0
Will Friebel <will.friebel@gmail.com>
2018-07-30 13:27:26 -0700

    Adjust app.js to work in light of file restructuring

--- server/app.js
+++ server/app.js
@@ -23,9 +23,9 @@ app.use('/auth', authRoutes);
 app.use('/api', passport.authenticate('jwt', { session: false }), apiRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client/build/index.html'));
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
     })
   }
 
